99ce8df5be391c154eb0180e2440d7d2f6e38b1e
Timaz69 <75456396+Timaz69@users.noreply.github.com>
2022-06-15 10:59:03 -0400

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2163,6 +2163,7 @@ class MaskRCNN():
 		optimizer = keras.optimizers.Adam(
 		    lr=learning_rate,
 		    clipnorm=self.config.GRADIENT_CLIP_NORM)
+	
         # Add Losses
         # First, clear previously set losses to avoid duplication
         self.keras_model._losses = []
